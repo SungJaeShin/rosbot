d6841a91728562a73d4b0a383719b916e9643d43
SungJaeShin <pootti@kaist.ac.kr>
2020-08-13 15:14:18 +0900

    sync ground truth

--- src/rosbot_multirobot_orientation/src/gt_to_tf.cpp
+++ src/rosbot_multirobot_orientation/src/gt_to_tf.cpp
@@ -19,21 +19,19 @@
 #include <Eigen/Geometry>
 #include "nav_msgs/Odometry.h"
 
-void Groundtruth1_callback(const nav_msgs::Odometry::ConstPtr& sub_msg){
-  geometry_msgs::Pose pose1 = sub_msg -> pose.pose;
-  Eigen::Matrix4f tf1 = unavlib::cvt::geoPose2eigen(pose1);
+void Groundtruth_callback(const rosbot_multirobot_orientation::SyncedClouds::ConstPtr& sub_msg){
+  nav_msgs::Odometry odom1 = sub_msg -> pose1;
+  nav_msgs::Odometry odom2 = sub_msg -> pose2;
+  nav_msgs::Odometry odom3 = sub_msg -> pose3;
+
+  Eigen::Matrix4f tf1 = unavlib::cvt::geoPose2eigen(odom1.pose.pose);
+  Eigen::Matrix4f tf2 = unavlib::cvt::geoPose2eigen(odom2.pose.pose);
+  Eigen::Matrix4f tf3 = unavlib::cvt::geoPose2eigen(odom3.pose.pose);
+
   std::cout << "-- Output of rosbot1 TF --" << std::endl;
   std::cout << tf1 << std::endl;
-}
-void Groundtruth2_callback(const nav_msgs::Odometry::ConstPtr& sub_msg){
-  geometry_msgs::Pose pose2 = sub_msg -> pose.pose;
-  Eigen::Matrix4f tf2 = unavlib::cvt::geoPose2eigen(pose2);
   std::cout << "-- Output of rosbot2 TF --" << std::endl;
   std::cout << tf2 << std::endl;
-}
-void Groundtruth3_callback(const nav_msgs::Odometry::ConstPtr& sub_msg){
-  geometry_msgs::Pose pose3 = sub_msg -> pose.pose;
-  Eigen::Matrix4f tf3 = unavlib::cvt::geoPose2eigen(pose3);
   std::cout << "-- Output of rosbot3 TF --" << std::endl;
   std::cout << tf3 << std::endl;
 }
@@ -44,12 +42,30 @@ int main(int argc, char** argv){
   ros::NodeHandle nh;
 
   // Subscriber Initializations to all ROSBOT 1-3s
-  ros::Subscriber sub1 = nh.subscribe("/rosbot1/ground_truth/state", 100, Groundtruth1_callback);
-  ros::Subscriber sub2 = nh.subscribe("/rosbot2/ground_truth/state", 100, Groundtruth2_callback);
-  ros::Subscriber sub3 = nh.subscribe("/rosbot3/ground_truth/state", 100, Groundtruth3_callback);
+  ros::Subscriber sub = nh.subscribe("/rosbots/clouds", 100, Groundtruth_callback);
 
   // Assign Publisher object with nodehandle
   ros::spin();
 
   return 0;
 }
+
+
+// void Groundtruth1_callback(const nav_msgs::Odometry::ConstPtr& sub_msg){
+//   geometry_msgs::Pose pose1 = sub_msg -> pose.pose;
+//   Eigen::Matrix4f tf1 = unavlib::cvt::geoPose2eigen(pose1);
+//   std::cout << "-- Output of rosbot1 TF --" << std::endl;
+//   std::cout << tf1 << std::endl;
+// }
+// void Groundtruth2_callback(const nav_msgs::Odometry::ConstPtr& sub_msg){
+//   geometry_msgs::Pose pose2 = sub_msg -> pose.pose;
+//   Eigen::Matrix4f tf2 = unavlib::cvt::geoPose2eigen(pose2);
+//   std::cout << "-- Output of rosbot2 TF --" << std::endl;
+//   std::cout << tf2 << std::endl;
+// }
+// void Groundtruth3_callback(const nav_msgs::Odometry::ConstPtr& sub_msg){
+//   geometry_msgs::Pose pose3 = sub_msg -> pose.pose;
+//   Eigen::Matrix4f tf3 = unavlib::cvt::geoPose2eigen(pose3);
+//   std::cout << "-- Output of rosbot3 TF --" << std::endl;
+//   std::cout << tf3 << std::endl;
+// }
--- src/rosbot_multirobot_orientation/src/sync_laserscans.cpp
+++ src/rosbot_multirobot_orientation/src/sync_laserscans.cpp
@@ -7,7 +7,7 @@
 #include <rosbot_multirobot_orientation/SyncedClouds.h>
 #include <tf/transform_listener.h>
 #include <laser_geometry/laser_geometry.h>
-
+#include "nav_msgs/Odometry.h"
 
 class SyncFilter {
 	public:
@@ -15,11 +15,17 @@ class SyncFilter {
 		void Laserscan1_callback(const sensor_msgs::LaserScan::ConstPtr& sub_msg);
 		void Laserscan2_callback(const sensor_msgs::LaserScan::ConstPtr& sub_msg);
 		void Laserscan3_callback(const sensor_msgs::LaserScan::ConstPtr& sub_msg);
+		void Groundtruth1_callback(const nav_msgs::Odometry::ConstPtr& sub_msg);
+		void Groundtruth2_callback(const nav_msgs::Odometry::ConstPtr& sub_msg);
+		void Groundtruth3_callback(const nav_msgs::Odometry::ConstPtr& sub_msg);
 
 	private:
 		ros::NodeHandle nh;
 		laser_geometry::LaserProjection projector_;
 		tf::TransformListener tfListener_;
+		double t_pc;
+		double t_odom;
+		double t_odom_next;
 
 		//Publishers and subscribers
 		ros::Publisher pub;
@@ -29,6 +35,9 @@ class SyncFilter {
 		ros::Subscriber sub1;
 		ros::Subscriber sub2;
 		ros::Subscriber sub3;
+		ros::Subscriber sub4;
+		ros::Subscriber sub5;
+		ros::Subscriber sub6;
 
 		//Messages
 		rosbot_multirobot_orientation::SyncedClouds pub_msg;
@@ -36,6 +45,9 @@ class SyncFilter {
 		sensor_msgs::PointCloud2 pub_msg_1cloud;
 		sensor_msgs::PointCloud2 pub_msg_2cloud;
 		sensor_msgs::PointCloud2 pub_msg_3cloud;
+		std::vector<std_msgs::Header> buf_pc;
+		std::vector<std_msgs::Header> buf_odom;
+
 };
 
 SyncFilter::SyncFilter(){
@@ -49,6 +61,9 @@ SyncFilter::SyncFilter(){
 	sub1 = nh.subscribe("/rosbot1/scan", 100, &SyncFilter::Laserscan1_callback, this);
 	sub2 = nh.subscribe("/rosbot2/scan", 100, &SyncFilter::Laserscan2_callback, this);
 	sub3 = nh.subscribe("/rosbot3/scan", 100, &SyncFilter::Laserscan3_callback, this);
+  sub4 = nh.subscribe("/rosbot1/ground_truth/state", 100, &SyncFilter::Groundtruth1_callback, this);
+	sub5 = nh.subscribe("/rosbot2/ground_truth/state", 100, &SyncFilter::Groundtruth2_callback, this);
+	sub6 = nh.subscribe("/rosbot3/ground_truth/state", 100, &SyncFilter::Groundtruth3_callback, this);
 
 }
 
@@ -75,7 +90,7 @@ void SyncFilter::Laserscan2_callback(const sensor_msgs::LaserScan::ConstPtr& sub
 
 	std::mutex mtx;
     mtx.lock();
-    
+
     pub_msg.pc2 = cloud;
 
     mtx.unlock();
@@ -91,9 +106,9 @@ void SyncFilter::Laserscan3_callback(const sensor_msgs::LaserScan::ConstPtr& sub
 
 	std::mutex mtx;
     mtx.lock();
-    
+
     pub_msg.pc3 = cloud;
-    
+
     mtx.unlock();
     pub_msg_3cloud = cloud;
     pub1_debug.publish(pub_msg_3cloud);
@@ -101,22 +116,86 @@ void SyncFilter::Laserscan3_callback(const sensor_msgs::LaserScan::ConstPtr& sub
     bool is_pc1_updated = pub_msg.pc1.header.stamp != old_pub_msg.pc1.header.stamp;
     bool is_pc2_updated = pub_msg.pc2.header.stamp != old_pub_msg.pc2.header.stamp;
 
-    if(is_pc1_updated && is_pc2_updated){
-    	pub_msg.header.stamp = ros::Time::now();
-    	pub.publish(pub_msg);
-    	old_pub_msg = pub_msg;
-    }
+		if(is_pc1_updated && is_pc2_updated){
+			std_msgs::Header tmp;
+			tmp.stamp = ros::Time::now();
+			buf_pc.emplace_back(tmp);
+		}
+
+		if(buf_pc.size() > 1 && buf_odom.size() > 1){
+			t_pc = buf_pc.front().stamp.toSec();
+			bool is_publish = true;
+			int odom_point = 0;
+
+			while(is_publish){
+				t_odom = buf_odom[odom_point].stamp.toSec();
+				t_odom_next = buf_odom[odom_point + 1].stamp.toSec();
+
+				if(t_odom < t_pc && t_pc < t_odom_next){
+					pub_msg.header.stamp = ros::Time::now();
+					pub.publish(pub_msg);
+					old_pub_msg = pub_msg;
+
+					is_publish = false;
+					buf_pc.clear();
+					buf_odom.clear();
+				}
+
+				odom_point ++;
+			}
+
+		}
+    // if(is_pc1_updated && is_pc2_updated){
+    // 	pub_msg.header.stamp = ros::Time::now();
+    // 	pub.publish(pub_msg);
+    // 	old_pub_msg = pub_msg;
+    // }
+
 }
 
+void SyncFilter::Groundtruth1_callback(const nav_msgs::Odometry::ConstPtr& sub_msg){
+  nav_msgs::Odometry pose_1 = *sub_msg;
 
+	std::mutex mtx;
+	mtx.lock();
+	pub_msg.pose1 = pose_1;
+	mtx.unlock();
 
+}
+void SyncFilter::Groundtruth2_callback(const nav_msgs::Odometry::ConstPtr& sub_msg){
+  nav_msgs::Odometry pose_2 = *sub_msg;
+
+	std::mutex mtx;
+	mtx.lock();
+	pub_msg.pose2 = pose_2;
+	mtx.unlock();
+
+}
+void SyncFilter::Groundtruth3_callback(const nav_msgs::Odometry::ConstPtr& sub_msg){
+  // nav_msgs::Odometry pose_3 = sub_msg;
+
+	std::mutex mtx;
+	mtx.lock();
+	pub_msg.pose3 = *sub_msg;
+	mtx.unlock();
+
+	bool is_gt1_updated = pub_msg.pose1.header.stamp != old_pub_msg.pose1.header.stamp;
+	bool is_gt2_updated = pub_msg.pose2.header.stamp != old_pub_msg.pose2.header.stamp;
+
+  if(is_gt1_updated && is_gt2_updated){
+		std_msgs::Header tmp;
+		tmp.stamp = ros::Time::now();
+		buf_odom.emplace_back(tmp);
+	}
+
+}
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "sync_laserscans");
-	
+
 	SyncFilter syncfilter;
-	
+
 	ros::spin();
 
 	return 0;
